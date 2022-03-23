91b23d1e57e9062f4248ebf14dce2b26ae41dcee
Luis Murillo Baltodano <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2022-03-23 15:46:42 -0600

    Update getInterpolatedColor.ts

--- ts/getInterpolatedColor.ts
+++ ts/getInterpolatedColor.ts
@@ -1,24 +1,49 @@
-type ColorQuartet = [number,number,number,number]
-
-function getInterpolatedColor(
-    frecuency: number,
-    colorQuartetA: ColorQuartet,
-    colorQuartetB: ColorQuartet
-) {
-    const normalizedTop = Math.min(1, frecuency / 128);
-    
-    const [red, green, blue, alpha] = colorQuartetA.map((value, index) => (
-        normalizedTop * (
-            Math.max(
-                value, colorQuartetB[index]
-            ) - Math.min(
-                    value, colorQuartetB[index]
-                )
-            ) + Math.min(
-                value, colorQuartetB[index]
-            )
-        )
-    );
-
-    return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+/**
+ * @param bottom: less than top
+ */
+function cornerValue(bottom: number, top: number) {
+  return function transformValue(value) {
+    return Math.max(Math.min(value, top), bottom);
+  };
 }
+
+function fitValue(sideA: number, sideB: number) {
+  return function transformValue(value) {
+    return (
+      value * (Math.max(sideA, sideB) - Math.min(sideA, sideB)) +
+      Math.min(sideA, sideB)
+    );
+  };
+}
+
+type Quartet = [number, number, number, number];
+
+/**
+ * @param frecuency: 0 to 1
+ * @param colorQuartet: [0 to 255, 0 to 255, 0 to 255, 0 to 1]
+ */
+function getInterpolatedColor(
+  frecuency: number,
+  colorQuartetA: Quartet,
+  colorQuartetB: Quartet
+) {
+  const alphaIndex = 3;
+  const [colorBottom, colorTop] = [0, 255];
+  const [red, green, blue, alpha] = colorQuartetA.map((value, index) => {
+    const interpolation = fitValue(value, colorQuartetB[index])(frecuency);
+
+    return index < alphaIndex
+      ? cornerValue(colorBottom, colorTop)(Math.floor(interpolation))
+      : interpolation;
+  });
+
+  return `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+}
+
+/*
+getInterpolatedColor(
+    .5,
+    [100, 200, 55, 1],
+    [200, 255, 20, .3]
+)
+*/
