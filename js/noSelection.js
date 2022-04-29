233a6eccdac71062c6fb7e428b452f194d9ed45d
Luis Murillo Baltodano <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2022-04-29 01:00:12 -0600

    Create noSelection.js

---
+++ js/noSelection.js
@@ -0,0 +1,42 @@
+// http://etimologias.dechile.net/
+
+document.onkeypress = function (event) {
+    event = (event || window.event);
+    if (event.keyCode == 123) {
+       //alert('No F-12');
+        return false;
+    }
+}
+document.onmousedown = function (event) {
+    event = (event || window.event);
+    if (event.keyCode == 123) {
+        //alert('No F-keys');
+        return false;
+    }else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+    return false;
+    }
+}
+document.onkeydown = function (event) {
+   event = (event || window.event);
+  
+   if (event.keyCode == 123) {
+        return false;
+   } 
+  
+   if(event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) {
+          return false;
+   }
+  
+   if(event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) {
+          return false;
+   }
+  
+   if(event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) {
+         return false;
+   }
+  
+   if(event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+   }
+}
+
