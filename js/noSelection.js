1664190e832cfb29f440b46bf4e014823e4c4bc9
Luis Murillo Baltodano <37167842+LuisArmando-TestCoder@users.noreply.github.com>
2022-04-29 01:14:04 -0600

    Update noSelection.js

--- js/noSelection.js
+++ js/noSelection.js
@@ -1,40 +1,47 @@
-document.onkeypress = function (event) {
-    event = (event || window.event);
-    if (event.keyCode == 123) {
-       //alert('No F-12');
-        return false;
-    }
+function isKey(event, key) {
+    return event.key.toUppercase() == key
 }
-document.onmousedown = function (event) {
-    event = (event || window.event);
-    if (event.keyCode == 123) {
-        //alert('No F-keys');
-        return false;
-    }else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
-    return false;
-    }
-}
-document.onkeydown = function (event) {
-   event = (event || window.event);
-  
-   if (event.keyCode == 123) {
-        return false;
-   } 
-  
-   if(event.ctrlKey && event.shiftKey && event.keyCode == 'I'.charCodeAt(0)) {
-          return false;
-   }
-  
-   if(event.ctrlKey && event.shiftKey && event.keyCode == 'C'.charCodeAt(0)) {
-          return false;
-   }
-  
-   if(event.ctrlKey && event.shiftKey && event.keyCode == 'J'.charCodeAt(0)) {
-         return false;
-   }
-  
-   if(event.ctrlKey && event.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-   }
-}
+
+document.addEventListener('keypress', (event) => {
+  event = event || window.event
+  if (event.key == 'F12') {
+    // No F-12
+    return false
+  }
+})
+
+document.addEventListener('mousedown', (event) => {
+  event = event || window.event
+  if (event.key == 'F12') {
+    return false
+  } else if (event.ctrlKey && event.shiftKey && isKey(event, 'I')) {
+    return false
+  }
+})
+
+document.addEventListener('keydown', (event) => {
+  event = event || window.event
+
+  if (isKey(event, 'F12')) {
+    return false
+  }
+
+  const areShiftAndControlPressed = event.ctrlKey && event.shiftKey
+
+  if (areShiftAndControlPressed && isKey(event, 'I')) {
+    return false
+  }
+
+  if (areShiftAndControlPressed && isKey(event, 'C')) {
+    return false
+  }
+
+  if (areShiftAndControlPressed && isKey(event, 'J')) {
+    return false
+  }
+
+  if (event.ctrlKey && isKey(event, 'U')) {
+    return false
+  }
+})
 
